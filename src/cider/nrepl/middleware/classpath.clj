c4e0eae7098b7f85e848f127fac3253869c9d048
sanjayl <sanjay.linganna@gmail.com>
2016-04-15 16:26:07 -0400

    Circular dependency op

--- src/cider/nrepl/middleware/classpath.clj
+++ src/cider/nrepl/middleware/classpath.clj
@@ -3,10 +3,26 @@
             [clojure.java.classpath :as cp]
             [clojure.tools.nrepl.middleware :refer [set-descriptor!]]
             [clojure.tools.namespace.dir :as dir]
+            [clojure.tools.namespace.file :as file]
             [clojure.tools.namespace.track :as track]))
 
 (defn classpath []
   (map str (cp/classpath)))
+
+(defn find-cycles [cur {:keys [seen root stack graph] :as state}]
+  (first (filter identity (for [c (remove seen cur)]
+                            (if (= c root)
+                              (conj stack c)
+                              (find-cycles (get graph c) (-> state
+                                                             (update-in [:stack] conj c)
+                                                             (update-in [:seen] conj c))))))))
+
+(defn cycle-paths [graph]
+  (filterv identity
+           (for [[root deps] graph
+                 :let [stack (find-cycles deps {:seen #{} :stack [root] :graph graph :root root})]]
+             stack)))
+
 
 (defn classpath-reply [msg]
   {:classpath (classpath)})
@@ -30,13 +46,31 @@
     (sort-by first $)                       ;; '([ns1 #{sorted-ns1-deps}], [ns2 #{sorted-ns2-deps}], ... )
     {:dependencies $}))
 
+(defn circular-reply
+  "Finds all distinct circular dependencies on the classpath and
+  returns them. This uses private, internal functions of
+  `clojure.tools.namespace` and so it has a higher likihood of
+  breaking than other functions."
+  [& _]
+  (as-> (#'dir/dirs-on-classpath) $
+    (#'dir/find-files $)
+    (#'file/files-and-deps $)
+    (:depmap $)
+    (cycle-paths $)
+    (map (fn [v] [(set v) v]) $)   ;; uniqify
+    (into {} $)  ;; uniqify
+    (vals $)
+    (map (fn [c] ["Circular Dependency Error" c]) $)
+    {:dependencies $}))
+
 (defn wrap-classpath
   "Middleware that provides the java classpath as well as dependency
   information."
   [handler]
   (with-safe-transport handler
     "classpath" classpath-reply
-    "dependencies" dependencies-reply))
+    "dependencies" dependencies-reply
+    "circular" circular-reply))
 
 (set-descriptor!
  #'wrap-classpath
@@ -49,4 +83,10 @@
     :optional {"ns" "The name of a single namespace to collect dependency information for."}
     :returns {"dependencies"
               "List of pairs, [head -> ns, tail -> ns's dependencies]."
+              "status" "done"}}
+   "circular"
+   {:doc "Obtains a dependency tree for the namespaces with circular dependencies on the classpath."
+    :optional {"ns" "The name of a single namespace to collect dependency information for."}
+    :returns {"dependencies"
+              "List of pairs, [head -> \"Circular Dependency\", tail -> ns path of circular dependency]."
               "status" "done"}}}})
