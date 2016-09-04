68f8d84cb68118e1374538a246acc19e7843fd48
sanjayl <sanjay.linganna@gmail.com>
2016-09-03 22:40:36 -0400

    Cleanup of old code

--- src/cider/nrepl/middleware/xref.clj
+++ src/cider/nrepl/middleware/xref.clj
@@ -1,5 +1,4 @@
 (ns cider.nrepl.middleware.xref
-  (:require [clojure.walk :as walk])
   (:import (clojure.lang RT)
            (java.io LineNumberReader InputStreamReader Reader PushbackReader)))
 
@@ -9,62 +8,35 @@
   a namespace for which the .clj is in the classpath. Returns nil if
   it can't find the source.
   Example: (get-source-from-var 'filter)"
-
-  [v] (when-let [filepath (:file (meta v))]
-        (when-let [strm (.getResourceAsStream (RT/baseLoader) filepath)]
-          (with-open [rdr (LineNumberReader. (InputStreamReader. strm))]
-            (dotimes [_ (dec (:line (meta v)))] (.readLine rdr))
-            (let [text (StringBuilder.)
-                  pbr (proxy [PushbackReader] [rdr]
-                        (read [] (let [#^Reader this this
-                                       i (proxy-super read)]
-                                   (.append text (char i))
-                                   i)))]
-              (read (PushbackReader. pbr))
-              (str text))))))
-
-
+  [v]
+  (when-let [filepath (:file (meta v))]
+    (when-let [strm (.getResourceAsStream (RT/baseLoader) filepath)]
+      (with-open [rdr (LineNumberReader. (InputStreamReader. strm))]
+        (dotimes [_ (dec (:line (meta v)))] (.readLine rdr))
+        (let [text (StringBuilder.)
+              pbr (proxy [PushbackReader] [rdr]
+                    (read [] (let [#^Reader this this
+                                   i (proxy-super read)]
+                               (.append text (char i))
+                               i)))]
+          (read (PushbackReader. pbr))
+          (str text))))))
 
 (defn- recursive-contains? [coll obj]
   "True if coll contains obj. Obj can't be a seq"
   (not (empty? (filter #(= obj %) (flatten coll)))))
 
-
 (defn- does-var-call-fn [var fn]
   "Checks if a var calls a function named 'fn"
-  (if-let [source (get-source-from-var var)]
-    (let [node (read-string source)]
-      (if (recursive-contains? node fn)
-        var
-        false))))
-
-(defn- does-ns-refer-to-var? [ns var]
-  (ns-resolve ns var))
-
-(defn all-vars-who-call-2 [sym]
-  (filter
-   ifn?
-   (filter
-    #(identity %)
-    (map #(does-var-call-fn % sym)
-         (flatten
-          (map vals
-               (map ns-interns
-                    (filter #(does-ns-refer-to-var? % sym)
-                            (all-ns)))))))))
-
+  (when-let [source (get-source-from-var var)]
+    (when (recursive-contains? (read-string source) fn)
+      var)))
 
 (defn all-vars-who-call [sym]
   (as-> (all-ns)
       $
-    (filter #(does-ns-refer-to-var? % sym) $)
-    (map ns-interns $)
-    (map vals $)
+    (filter #(ns-resolve % sym) $)
+    (map (comp vals ns-interns) $)
     (flatten $)
     (map #(does-var-call-fn % sym) $)
-    (filter #(identity %) $)
     (filter ifn? $)))
-
-(as-> (all-ns)
-    $
-  (filter #(ns-resolve % sym) $))
