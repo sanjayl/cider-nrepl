878ece2bc974040f94bc47db5c23bb36f6fec9a8
sanjayl <sanjay.linganna@gmail.com>
2016-09-08 23:14:03 -0400

    More robust version of rewrite
    Fixes issue with a single namespace spread out over multiple files (ie,
    clojure.core) as well as the issue with not being able to flatten the
    source code completely if there were maps involved.

--- src/cider/nrepl/middleware/xref.clj
+++ src/cider/nrepl/middleware/xref.clj
@@ -3,29 +3,22 @@
   (:import (clojure.lang RT)
            (java.io LineNumberReader InputStreamReader Reader PushbackReader)))
 
-(defn- namespaces-that-call [sym]
-  (->> (all-ns)
-       (filter #(ns-resolve % sym))))
-
-#_(defn- meta-with-source [sym]
-    (assoc (meta sym) :source (source-code sym)))
-
-(defn- fns-in-ns-that-call [ns target]
-  (let [fns (->> ns ns-interns (filter ifn?))
-        source (->> fns first :meta :source)]))
-
-(defn- all-fns-in [ns]
-  (->> ns ns-interns (map second) (filter ifn?)))
-
-(defn- assoc-meta-with-fn [fn]
+(defn- assoc-meta-with-fn
+  "Takes a resolved function var FN, and returns an associative with
+  some meta-data stuffed in."
+  [fn]
   (let [m (meta fn)]
     (hash-map :fn fn
-              ;;:meta m  ;; TODO GET RID OF THIS??
+              :meta m
               :file (:file m)
               :line (:line m)
               :column  (:column m))))
 
-(defn- read-source-from-a-file [[path raw-fns]]
+(defn- read-source-from-a-file
+  "Reads in a resource file specified by PATH, and parses Clojure
+  objects specified by the list of function-info provided by
+  RAW-FNS. TODO: Likely needs error handling."
+  [[path raw-fns]]
   (let [fns        (remove #(nil? (:line %)) raw-fns)
         sorted-fns (sort-by (juxt :line :column) fns)]
     (when-let [uri (and path (io/resource path))]
@@ -41,94 +34,54 @@
                   target  (:line item)]
               (dotimes [_ (- target current 1)] (.readLine lr)) ;; burn lines
               (recur (rest todo)
-                     (conj acc (assoc item :source (binding [*read-eval* false]
+                     (conj acc (assoc item :source (binding [*read-eval* false] ;; error handling here?
                                                      (read pbr))))))))))))
 
-(defn- does-fn-call? [fn-with-source target]
-  (some #{target} (-> fn-with-source :source flatten)))
+(defn- smash
+  "Aggressively flattens collection X. In contrast to
+  `clojure.core/flatten`, `smash` will flatten maps."
+  [x]
+  (remove coll? (rest (tree-seq coll? seq x))))
 
-(defn- process-namespace [ns target]
-  (as-> (all-fns-in ns) $              ; (fn1, fn2, fn3 ...)
-    (map assoc-meta-with-fn $)         ; ({:fn fn1 :meta m1 :file f1 :line l1}, {:fn fn2 :meta m2 :file f2 :line l2} ...)
-    (group-by :file $)                 ; ({file1 [{:fn 1..} {:fn 3..} {:fn 5..}]}, {file2 [{:fn 2..} {:fn 4..} {:fn 6..}]})
-    (mapcat read-source-from-a-file $) ; [{:fn 1 :m m1 :source s1}, {:fn 2 :m m2 :source s2} ...]
-    (filter #(does-fn-call? % target) $)))
+(defn- try-to-resolve
+  "Tries to resolve item X in namespace NS. Will return item X
+  unchanged if X is not a symbol."
+  [ns x]
+  (if (symbol? x) (ns-resolve ns x) x))
 
+(defn- fn-uses-var?
+  "Predicate that returns `true` if function FN-INFO's source-code
+  utilizes the var V."
+  [fn-info v]
+  (let [ns              (->> fn-info :meta :ns)
+        flat-source     (->> fn-info :source smash)
+        resolved-source (map #(try-to-resolve ns %) flat-source)]
+    (some #{v} resolved-source)))
 
-(defn callers-of [t]
-  (let [target (symbol t)]
-    (->> (namespaces-that-call target)
-         (mapcat #(process-namespace % target)))))
+(defn- fns-in-ns-that-use
+  "Finds all functions in the given namespace NS, reads in their
+  source code, resolves the symbols to vars, and checks to see if the
+  given var V matches any of the sourcecode."
+  [ns v]
+  (->> (->> ns ns-interns (map second) (filter ifn?)) ; all fns in ns
+       (map assoc-meta-with-fn)          ; Keys- :fn, :meta, :file, :line, :column
+       (group-by :file)                  ; One ns can be split over many files (clojure.core)
+       (mapcat read-source-from-a-file)  ; Adds :source to the above keys
+       (filter #(fn-uses-var? % v))))    ; Does var occur in the fn's source?
 
+(defn- does-this-ns-alias-that-that-ns?
+  "Predicate that returns `true` if the first namespace (THIS)
+  contains an alias to the second namespace (THAT)."
+  [this that]
+  (->> this ns-aliases vals (some #{that})))
 
-
-(comment (as-> (all-fns-in 'cider.nrepl.middleware.format) $ ; (fn1, fn2, fn3 ...)
-           (map assoc-meta-with-fn $) ; ({:fn fn1 :meta m1 :file f1 :line l1}, {:fn fn2 :meta m2 :file f2 :line l2} ...)
-           (group-by :file $) ; ({file1 [{:fn 1..} {:fn 3..} {:fn 5..}]}, {file2 [{:fn 2..} {:fn 4..} {:fn 6..}]})
-           (mapcat read-source-from-a-file $) ; [{:fn 1 :m m1 :source s1}, {:fn 2 :m m2 :source s2} ...]
-           (filter #(does-fn-call? % 'string/join) $)))
-
-
-
-(comment 
-
-  (defn callers-of [t]
-    (let [target (symbol t)]
-      (->> (namespaces-that-call target)
-           (mapcat #(fns-in-ns-that-call % target))
-           (map meta-with-source)
-           (filter #(some #{target} (-> % :source flatten))))))
-
-
-
-  (let [ns        (find-ns 'cider.nrepl.middleware.format)
-        fns       (->> ns ns-interns vals (filter ifn?))
-        ns-path   (->> fns first meta :file)
-        fn-w-meta (map #(hash-map :fn % :meta (meta %) :line (:line (meta %))) fns) ;;can we remove the :meta?
-        fn-lines  (->> fn-w-meta
-                       (remove #(nil? (:line %)))
-                       (sort-by :line))]
-    (when-let [ns-uri (and ns-path (io/resource ns-path))]
-      (with-open [r   (io/reader ns-uri)
-                  lr  (LineNumberReader. r)
-                  pbr (PushbackReader. lr)]
-        (loop [rem fn-lines
-               acc '()]
-          (if (empty? rem)
-            acc
-            (let [item    (first rem)
-                  current (.getLineNumber lr)
-                  target  (:line item)]
-              (dotimes [_ (- target current 1)] (.readLine lr))
-              (recur (rest rem)
-                     (conj acc (assoc item :source (binding [*read-eval* false]
-                                                     (read pbr)))))))))))
-
-
-
-
-  (let [ns        (find-ns 'clojure.core)
-        fns       (->> ns ns-interns vals (filter ifn?))
-        ns-path   (->> fns first meta :file)
-        fn-w-meta (map #(hash-map :fn % :meta (meta %) :line (:line (meta %))) fns) ;;can we remove the :meta?
-        fn-lines  (->> fn-w-meta
-                       (remove #(nil? (:line %)))
-                       (sort-by :line))]
-    (->> (group-by #(get-in % [:meta :file]) fn-lines)
-         first
-         val)
-    #_(when-let [ns-uri (and ns-path (io/resource ns-path))]
-        (with-open [r   (io/reader ns-uri)
-                    lr  (LineNumberReader. r)
-                    pbr (PushbackReader. lr)]
-          (loop [rem fn-lines
-                 acc '()]
-            (if (empty? rem)
-              acc
-              (let [item    (first rem)
-                    current (.getLineNumber lr)
-                    target  (:line item)]
-                (dotimes [_ (- target current 1)] (.readLine lr))
-                (recur (rest rem)
-                       (conj acc (assoc item :source (binding [*read-eval* false]
-                                                       (read pbr))))))))))))
+(defn callers-of
+  "Pass in a fully namespace QUALIFIED-NAME, and get a collection of
+  maps, each map detailing a function that calls QUALIFIED-NAME. N.B.,
+  currently, will not be able to find cross references for functions
+  in `clojure.core`."
+  [qualified-name]
+  (let [v (resolve (symbol qualified-name))]
+    (->> (all-ns)
+         (filter #(does-this-ns-alias-that-that-ns? % (-> v meta :ns))) ; Narrow search space
+         (mapcat #(fns-in-ns-that-use % v))))) ; Searches the source code and returns the xrefs
